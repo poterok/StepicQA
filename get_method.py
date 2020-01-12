b4d848d892667340d35c363dc29aa7ced10bb2dd
poterok <poterok@gmail.com>
2020-01-12 12:27:33 +0300

    Добавил код для урока 2.1.4
    Все работает но не могу вытащить значение X

---
+++ get_method.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import time
+import math
+
+# webdriver это и есть набор команд для управления браузером
+from selenium import webdriver
+
+# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+driver = webdriver.Chrome()
+
+# команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
+time.sleep(1)
+
+# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
+# driver.get("https://stepik.org/lesson/25969/step/12")
+driver.get("http://suninjuly.github.io/math.html")
+# time.sleep(5)
+
+
+# Передаем значение в функцию
+# cosx = driver.find_element_by_xpath(["input_value"])
+# print(cosx)
+x = 112
+
+# def calc(x):
+#   return str(math.log(abs(12*math.sin(int(x)))))
+
+# Метод find_element_by_css_selector позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
+# Ищем поле для ввода текста
+textarea = driver.find_element_by_id("answer")
+
+
+
+# Напишем текст ответа в найденное поле
+textarea.send_keys(x)
+
+
+checkbox = driver.find_element_by_id("robotCheckbox")
+checkbox.click()
+
+radiobutton1 = driver.find_element_by_id("robotsRule")
+radiobutton1.click()
+
+
+
+# Найдем кнопку, которая отправляет введенное решение
+submit_button = driver.find_element_by_class_name("btn-default")
+submit_button.click()
+
+# Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
+time.sleep(5)
+
+# После выполнения всех действий мы не должны забыть закрыть окно браузера
+driver.quit()
+
